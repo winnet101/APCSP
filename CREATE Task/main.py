f4890abc8bd4a02b5c1387f6576050bd21b1bed2
winnet101 <115325533+winnet101@users.noreply.github.com>
2024-03-12 11:03:23 -0700

    a

--- CREATE Task/main.py
+++ CREATE Task/main.py
@@ -1,16 +1,19 @@
 import turtle as trtl 
 from PIL import Image, ImageOps
 import os
-from utils import draw_rect, quickmove
+from utils import draw_rect, quickmove, abs_resize, clear_folder
 
-cookie_img = "cookie.gif"
+cookie_img = "assets/cookie.gif"
+COOKIE_IMG_PATH = "cookie_cache"
+ICON_IMG_PATH = "icon_cache"
 
 cookies = 0
 
 # init screen
 trtl.tracer(0)
 wn = trtl.Screen()
-wn.setup(0.5, 1.0, 630)
+# wn.setup(0.5, 1.0, 630)
+wn.setup(1.0, 1.0)
 wn.addshape(cookie_img)
 
 cookie = trtl.Turtle()
@@ -34,28 +37,41 @@ for building in buildings:
   building_turtles.append(trtl.Turtle())
 
 building_costs:list[int] = []
+for building in buildings:
+  match building:
+    case "cursor":
+      building_costs.append(10)
+    case "grandma":
+      building_costs.append(100)
+    case "infiniplex":
+      building_costs.append(10000)
+    case _:
+      print(f"[BUILDING_COSTS]: Building {building} is not defined.")
 
-for file in os.listdir("assets"):
-  file_path = os.path.join("assets/", file)
-  os.unlink(file_path)
+building_effects:list[float] = []
+for building in buildings:
+  match building:
+    case "cursor":
+      building_effects.append(0.1)
+    case "grandma":
+      building_effects.append(1)
+    case "infiniplex":
+      building_effects.append(100)
+    case _:
+      print(f"[BUILDING_EFFECTS]: Building {building} is not defined.")
+
+clear_folder(COOKIE_IMG_PATH)
+clear_folder(ICON_IMG_PATH)
 
 prev_size:tuple[int, int] = (cookie_size, cookie_size)
 is_animating = False
 
 def abs_resize_cookie(resize:int):
-  with Image.open(cookie_img) as im:
-    global prev_size
-    new_size_arr:list = [resize, resize]
-    new_size = tuple(new_size_arr)
-    prev_size = new_size
-
-    new_img = f"assets/cookie{new_size[0]}.gif"
-
-    ImageOps.contain(im, new_size).save(new_img)
-
-  wn.addshape(new_img)
-  cookie.shape(new_img)
-  wn.update()
+  global prev_size
+  abs_resize(cookie, cookie_img, resize, COOKIE_IMG_PATH)
+  new_size_arr:list = [resize, resize]
+  new_size = tuple(new_size_arr)
+  prev_size = new_size
 
 # randomize size/time
 def bounce_cookie(init_size: int):
@@ -76,36 +92,60 @@ def bounce_cookie(init_size: int):
     wn.update()
   is_animating = False
 
-def update_score(new_score: int):
+def update_cookies(new_score: int):
   global cookies
   cookies_writer.clear()
   cookies += new_score
-  cookies_writer.write(cookies, font=("Times New Roman", 20, "normal"))
+  cookies_writer.write(cookies, font=("Verdana", 20, "normal"))
   wn.update()
 
 def handle_cookie_click(x, y):
   global cookies
-  update_score(1)
+  update_cookies(1)
   bounce_cookie(cookie_size)
 
-def draw_button(t:trtl.Turtle, x:int, y:int, building:str): 
-  t.color("brown") 
-  new_button = tuple(draw_rect(t, x, y, 250, 100))
-  
-  t.color("black")
-  quickmove(t, int(t.xcor()) + 10, y - 25)
-  t.write(building.capitalize(), font=("Times New Roman", 30, "normal"))
+def get_building_cost(building: str):
+  return building_costs[[building for building, _number in buildings.items()].index(building)]
 
-  def handle_button_click(x, y):
-    if (new_button[0] < x < new_button[2]) and (new_button[3] < y < new_button[1]):
-      print(building)
+def get_building_effect(building: str):
+  return building_effects[[building for building, _number in buildings.items()].index(building)]
+
+def draw_button(t:trtl.Turtle, x:int, y:int, building:str): 
+  
+  t.color("#87481e") 
+  new_button = tuple(draw_rect(t, x, y, 400, 100))
+  
+  # image
+  t.color("white")
+  quickmove(t, int(t.xcor()) + 50, y)
+  abs_resize(t, f"assets/{building}.gif", 90, ICON_IMG_PATH)
+  t.stamp()
+
+  # name
+  quickmove(t, int(t.xcor()) + 70, y - 25)
+  t.write(building.capitalize(), font=("Verdana", 30, "bold"))
+
+  # cost
+  quickmove(t, int(t.xcor()), int(t.ycor()) - 15)
+  t.write(f"Cost: {get_building_cost(building)}", font=("Verdana", 10, "normal"))
+
+  quickmove(t, x + 100, y - 30)
+  t.write(buildings.get(building), font=("Verdana", 20, "normal"))
+
+  def handle_button_click(xclick, yclick):
+    if (new_button[0] < xclick < new_button[2]) and (new_button[3] < yclick < new_button[1]):
+      if cookies >= get_building_cost(building):
+        update_cookies(-get_building_cost(building))
+        buildings[building] += 1
+        draw_button(t, x, y, building)
 
   wn.onscreenclick(handle_button_click, add=True)
 
 for i, building in enumerate(buildings):
-  draw_button(building_turtles[i], 0, (i * -100), building)
+  draw_button(building_turtles[i], 400, (i * -120) + 120, building)
 
 abs_resize_cookie(cookie_size)
+update_cookies(0)
 cookie.onclick(handle_cookie_click)
 
 wn.listen()
--- CREATE Task/utils.py
+++ CREATE Task/utils.py
@@ -1,4 +1,6 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
+from PIL import Image, ImageOps
+import os
 
 def quickmove(t:Turtle, x:int, y:int):
   '''Moves the turtle to a given location without pen.
@@ -9,7 +11,7 @@ def quickmove(t:Turtle, x:int, y:int):
 
 def draw_rect(t:Turtle, x:int, y:int, width:int, height: int | None = None):
   '''Draws a rectangle.
-  :returns: An array containing its boundaries (`[left, up, right, down]`).'''
+  :returns -  An array containing its boundaries (`[left, up, right, down]`).'''
   if height == None:
     height = width
   
@@ -38,3 +40,35 @@ def draw_rect(t:Turtle, x:int, y:int, width:int, height: int | None = None):
   t.end_fill()
 
   return walls
+
+def abs_resize(t:Turtle, img_path:str, new_size:int, NEW_IMG_FOLDER:str = "assets"):
+  '''Given an image path, resizes it and sets the given turtle to that image.
+  :returns - A tuple of the new size.'''
+  with Image.open(img_path) as im:
+    new_size_tuple = (new_size, new_size)
+
+    def get_img_name():
+      name = ""
+      true_path = img_path
+      if "/" in img_path:
+        true_path_arr = img_path.split("/")
+        true_path = true_path_arr[len(true_path_arr) - 1]
+      for char in true_path:
+        if char == ".":
+          return name
+        else:
+          name += char
+
+    new_img = f"{NEW_IMG_FOLDER}/{get_img_name()}{new_size}.gif"
+    ImageOps.contain(im, new_size_tuple).save(new_img)
+
+  t.screen.addshape(new_img)
+  t.shape(new_img)
+  t.screen.update()
+  return new_size_tuple
+
+def clear_folder(path:str):
+  '''A simplified version of https://stackoverflow.com/questions/185936/how-to-delete-the-contents-of-a-folder.'''
+  for file in os.listdir(path):
+    file_path = os.path.join(path, file)
+    os.unlink(file_path)
